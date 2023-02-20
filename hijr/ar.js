4c1299c2a8848e9eaaa4245d5e84452ea3973a49
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-20 17:15:53 +0300

    Update ar.js

--- hijr/ar.js
+++ hijr/ar.js
@@ -5,5 +5,5 @@ $('.my-date-ar').hijriDate({
             weekDayLang: 'ru',
             gregLang: 'ru',
             hijriLang: 'ru',
-            correction: +0
+            correction: +1
         });
