c5c7e04804b88e050f1f3d735c713d0edcaa1ea6
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-03-11 13:36:30 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 var doCache = true;
 
 // Name our cache
-var CACHE_NAME = 'my-pwa-cache-v3';
+var CACHE_NAME = 'my-pwa-cache-v4';
 
 // Delete old caches that are not our current one!
 self.addEventListener("activate", event => {
